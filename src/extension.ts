9e6f23e6a9115966fbe5d09c2583c1d78dd646fe
Shubh <shubhisroking@proton.me>
2023-09-01 13:37:38 +0530

    initial extension
    Signed-off-by: Shubh <shubhisroking@proton.me>

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,64 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  let isStreamerModeEnabled = vscode.workspace
+    .getConfiguration("vscode-streamer-mode")
+    .get<boolean>("enabled");
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-streamer-mode" is now active!');
+  vscode.workspace.onDidOpenTextDocument((document) => {
+    if (isStreamerModeEnabled && isEnvFile(document)) {
+      vscode.commands
+        .executeCommand("workbench.action.closeActiveEditor")
+        .then(() => {
+          vscode.window
+            .showWarningMessage(
+              "Opening .env files is not allowed while streamer mode is enabled.",
+              "Open Anyway"
+            )
+            .then((choice) => {
+              if (choice === "Open Anyway") {
+                vscode.workspace.openTextDocument(document.uri).then((doc) => {
+                  vscode.window.showTextDocument(doc);
+                });
+              }
+            });
+        });
+    }
+  });
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-streamer-mode.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-streamer-mode!');
-	});
+  context.subscriptions.push(
+    vscode.commands.registerCommand("vscode-streamer-mode.toggle", () => {
+      isStreamerModeEnabled = !isStreamerModeEnabled;
+      vscode.workspace
+        .getConfiguration()
+        .update(
+          "vscode-streamer-mode.enabled",
+          isStreamerModeEnabled,
+          vscode.ConfigurationTarget.Global
+        );
+      vscode.window.showInformationMessage(
+        `Streamer mode is now ${
+          isStreamerModeEnabled ? "enabled" : "disabled"
+        }.`
+      );
+    })
+  );
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(
+    vscode.commands.registerCommand("vscode-streamer-mode.disable", () => {
+      isStreamerModeEnabled = false;
+      vscode.workspace
+        .getConfiguration()
+        .update(
+          "vscode-streamer-mode.enabled",
+          isStreamerModeEnabled,
+          vscode.ConfigurationTarget.Global
+        );
+      vscode.window.showInformationMessage("Streamer mode is now disabled.");
+    })
+  );
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+function isEnvFile(document: vscode.TextDocument): boolean {
+  return document.fileName.endsWith(".env");
+}
