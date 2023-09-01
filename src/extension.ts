b38a740c4613848e524e9f72a8fd32a308d4ea5d
Shubh <shubhisroking@proton.me>
2023-09-01 15:18:11 +0530

    Refactor and optimize extension code
    This commit refactors the extension code to use separate classes for StreamerMode and SensitiveFile, which improves code organization and readability.
    It also optimizes the SensitiveFile class by using a Set to store the file extensions and a regular expression to match file names, which simplifies the code and improves performance.
    Additionally, it updates the toggle command to use the StreamerMode class and adds a getEnabled method to the class to simplify code elsewhere.
    Finally, it updates the onDidOpenTextDocument event to use the SensitiveFile class and adds a flag to keep the editor open when the user chooses to open a sensitive file.
    The commit also includes a minor change to reset the flag when the active editor changes.
    
    Signed-off-by: Shubh <shubhisroking@proton.me>

--- src/extension.ts
+++ src/extension.ts
@@ -1,52 +1,105 @@
 import * as vscode from "vscode";
 
-let isStreamerModeEnabled = false;
+class StreamerMode {
+  private isEnabled = false;
+
+  constructor() {
+    vscode.workspace
+      .getConfiguration()
+      .update(
+        "vscode-streamer-mode.enabled",
+        this.isEnabled,
+        vscode.ConfigurationTarget.Global
+      );
+  }
+
+  public toggle() {
+    this.isEnabled = !this.isEnabled;
+    vscode.workspace
+      .getConfiguration()
+      .update(
+        "vscode-streamer-mode.enabled",
+        this.isEnabled,
+        vscode.ConfigurationTarget.Global
+      );
+    vscode.window.showInformationMessage(
+      `Streamer mode is now ${this.isEnabled ? "enabled" : "disabled"}.`
+    );
+  }
+
+  public getEnabled() {
+    return this.isEnabled;
+  }
+}
+
+class SensitiveFile {
+  private readonly fileExtensions: Set<string>;
+  private readonly regex: RegExp;
+
+  constructor(fileExtensions: string[]) {
+    this.fileExtensions = new Set(fileExtensions);
+    this.regex = new RegExp(`\\.(${fileExtensions.join("|")})$`, "i");
+  }
+
+  public isSensitiveFile(document: vscode.TextDocument): boolean {
+    return this.fileExtensions.has(this.getFileExtension(document.fileName));
+  }
+
+  private getFileExtension(fileName: string): string {
+    const lastDotIndex = fileName.lastIndexOf(".");
+    return lastDotIndex === -1 ? "" : fileName.slice(lastDotIndex + 1);
+  }
+}
+
+let shouldKeepEditorOpen = false;
 
 export function activate(context: vscode.ExtensionContext) {
+  const streamerMode = new StreamerMode();
+  const sensitiveFile = new SensitiveFile([
+    "env",
+    "local",
+    "development",
+    "staging",
+    "production",
+    "test",
+    "pem",
+    "key",
+    "cer",
+    "crt",
+    "p12",
+    "pfx",
+  ]);
+
   context.subscriptions.push(
     vscode.commands.registerCommand("vscode-streamer-mode.toggle", () => {
-      isStreamerModeEnabled = !isStreamerModeEnabled;
-      vscode.workspace
-        .getConfiguration()
-        .update(
-          "vscode-streamer-mode.enabled",
-          isStreamerModeEnabled,
-          vscode.ConfigurationTarget.Global
-        );
-      vscode.window.showInformationMessage(
-        `Streamer mode is now ${
-          isStreamerModeEnabled ? "enabled" : "disabled"
-        }.`
-      );
+      streamerMode.toggle();
     })
   );
 
   vscode.workspace.onDidOpenTextDocument((document) => {
-    if (isEnvFile(document) && isStreamerModeEnabled) {
-      vscode.window
-        .showWarningMessage(
-          "Opening .env files is not allowed in streamer mode.",
-          "Open Anyway"
-        )
-        .then((selectedOption) => {
-          if (selectedOption === "Open Anyway") {
-            vscode.commands.executeCommand(
-              "vscode.open",
-              vscode.Uri.file(document.fileName)
-            );
-          }
-        });
-      vscode.commands.executeCommand("workbench.action.closeActiveEditor");
+    if (sensitiveFile.isSensitiveFile(document) && streamerMode.getEnabled()) {
+      if (!shouldKeepEditorOpen) {
+        vscode.window
+          .showWarningMessage(
+            "Opening sensitive files is not allowed in streamer mode.",
+            "Open Anyway"
+          )
+          .then((selectedOption) => {
+            if (selectedOption === "Open Anyway") {
+              shouldKeepEditorOpen = true;
+              vscode.commands.executeCommand(
+                "vscode.open",
+                vscode.Uri.file(document.fileName)
+              );
+            }
+          });
+        vscode.commands.executeCommand("workbench.action.closeActiveEditor");
+      }
     }
   });
-}
 
-function isEnvFile(document: vscode.TextDocument): boolean {
-  return (
-    document.fileName.match(/\.env$/i) !== null ||
-    document.fileName.match(/\.env\.local$/i) !== null ||
-    document.fileName.match(/\.env\.(development|staging|production)$/i) !==
-      null ||
-    document.fileName.match(/\.env\.test$/i) !== null
-  );
+  // Reset the flag when the active editor changes
+  vscode.window.onDidChangeActiveTextEditor(() => {
+    shouldKeepEditorOpen = false;
+  });
 }
